f2d660428104aaf84adef960a4e998262320b711
dgdeleonardis <diegowazzaap@hotmail.it>
2015-02-15 18:08:50 +0100

    Aggiunta Contatore Call Stack v1.0

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 int moltiplicazioneIterativa(int a, int b);
-int moltiplicazioneRicorsiva(int a, int b);
+int moltiplicazioneRicorsiva(int a, int b, int *cont);
 
 int main(int argc, char** argv) {
-    int a,b;
+    int a, b;
     int rIterativo, rRicorsivo;
     
     printf("Inserisci a, il primo fattore (intero): ");
@@ -14,18 +14,30 @@ int main(int argc, char** argv) {
     scanf("%d", &b);
     
     rIterativo = moltiplicazioneIterativa(a, b);
-    rRicorsivo = moltiplicazioneRicorsiva(a, b);
+    rRicorsivo = moltiplicazioneRicorsiva(a, b, &b);
     
     printf("%d * %d = %d (formulazione iterativa)\n", a, b, rIterativo);
-    printf("%d * %d = %d (formulazione iterativa)\n", a, b, rRicorsivo);
+    printf("%d * %d = %d (formulazione ricorsiva)\n", a, b, rRicorsivo);
     return (EXIT_SUCCESS);
 }
 
 int moltiplicazioneIterativa(int a, int b) {
-    // TODO Implementa il corpo della funzione
-    return -1;
+    int p = 0;
+    for(; b > 0; b--) {
+        p += a;
+    }
+    return p;
 }
-int moltiplicazioneRicorsiva(int a, int b) {
-    // TODO Implementa il corpo della funzione
-    return -1;
+int moltiplicazioneRicorsiva(int a, int b, int *cont) {
+    int ris = 0;
+    printf("chiamata numero %d\n", *cont - b);
+    
+    if (b == 0) {
+        
+       return ris; 
+    }
+    else {
+       ris =  a + moltiplicazioneRicorsiva( a, b - 1, cont);
+    }
 }
+// Stack Overflow :42988
